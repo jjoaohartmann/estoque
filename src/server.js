b95abd9dc91c91cd1600438cdea6ce7856f7cfe6
JOÃO VICTOR PRANGE HARTMANN <joao_v_hartmann@estudante.sesisenai.org.br>
2026-06-25 07:57:23 -0300

    subindo atividade

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { prisma } from "./lib/prisma.ts"
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+
+app.listen(PORT, ()=>{
+console.log("API rodante")
+
+})
+
